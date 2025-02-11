af80ede6e9e20139ac3ec849d81d5f5575caafbb
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-11 21:22:27 +0300

    Add list of words

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import words from "./wordList.json"
+
 function App() {
+  const [wordGuess, setWordGuess] = useState("empty");
+
+  function changeWord() {
+    setWordGuess(words[Math.floor(Math.random() * words.length)]);
+  }
+
+  console.log(wordGuess);
+  useEffect(() => {
+    changeWord();
+  console.log(wordGuess);
+  }, [])
   return <h1>Hi</h1>
 }
 
