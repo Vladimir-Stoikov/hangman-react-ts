20f70608d58734df1c86d6ae0c9a284f7ebed33f
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-12 21:28:49 +0300

    Add base styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import words from "./wordList.json"
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
 
 function App() {
-  const [wordGuess, setWordGuess] = useState("empty");
+  const [wordGuess, setWordGuess] = useState(words[Math.floor(Math.random() * words.length)]);
 
-  function changeWord() {
-    setWordGuess(words[Math.floor(Math.random() * words.length)]);
-  }
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  console.log(wordGuess);
-  useEffect(() => {
-    changeWord();
-  console.log(wordGuess);
-  }, [])
-  return <h1>Hi</h1>
+  return <div style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}>
+    <div style={{
+      fontSize: "2rem",
+      textAlign: "center"
+    }}>Lose Win</div>
+    <div>Hi {wordGuess}</div>
+    <HangmanDrawing />
+    <HangmanWord />
+    <Keyboard />
+  </div>
 }
 
 export default App
