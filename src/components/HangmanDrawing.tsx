83efd2feffb53c3296e2240337ebc78cfc25570c
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-13 22:54:40 +0300

    complete HangmanDrawing component

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,7 +1,86 @@
-import React from 'react'
+const HEAD = (
+  <div style={{
+    height: "50px",
+    width: "50px",
+    borderRadius: "50%",
+    border: "10px solid black",
+    position: "absolute",
+    top: "70px",
+    right: "-30px"
+  }}/>
+)
+
+const BODY = (
+  <div style={{
+    height: "100px",
+    width: "10px",
+    background: "black",
+    position: "absolute",
+    top: "140px",
+    right: "0"
+  }}/>
+)
+
+const RIGHT_ARM = (
+  <div style={{
+    height: "10px",
+    width: "100px",
+    background: "black",
+    position: "absolute",
+    top: "160px",
+    right: "-90px",
+    rotate: "-30deg",
+  }}/>
+)
+
+const LEFT_ARM = (
+  <div style={{
+    height: "10px",
+    width: "100px",
+    background: "black",
+    position: "absolute",
+    top: "160px",
+    right: "0px",
+    rotate: "30deg",
+  }}/>
+)
+
+const RIGHT_LEG = (
+  <div style={{
+    height: "10px",
+    width: "100px",
+    background: "black",
+    position: "absolute",
+    top: "270px",
+    right: "-70px",
+    rotate: "60deg",
+  }}/>
+)
+
+const LEFT_LEG = (
+  <div style={{
+    height: "10px",
+    width: "100px",
+    background: "black",
+    position: "absolute",
+    top: "270px",
+    right: "-20px",
+    rotate: "-60deg",
+  }}/>
+)
+
 
 export default function HangmanDrawing() {
-  return (
-    <div>HangmanDrawing</div>
-  )
+  return <div style={{position: "relative"}}>
+    {HEAD}
+    {BODY}
+    {RIGHT_ARM}
+    {LEFT_ARM}
+    {RIGHT_LEG}
+    {LEFT_LEG}
+    <div style={{ height: '70px', width: "10px", background: "black", position: "absolute", top: 0, right: 0}}/>
+    <div style={{ height: '10px', width: "200px", background: "black", marginLeft: "120px"}}/>
+    <div style={{ height: '400px', width: "10px", background: "black", marginLeft: "120px"}}/>
+    <div style={{ height: '10px', width: "250px", background: "black"}}/>
+  </div>
 }
