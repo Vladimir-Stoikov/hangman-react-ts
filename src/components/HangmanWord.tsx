8284f4df89f4a8edb3d7481c7688e0d30c48c74d
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-14 23:13:37 +0300

    complete HangmanWord component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
 export default function HangmanWord() {
+
+  const word = "test";
+  const guessedLetters = ["e", "s"]
+
   return (
-    <div>HangmanWord</div>
+    <div style={{display: "flex", gap: ".25rem", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"}}>
+      {word.split("").map((letter, index) => (
+        <span key={index} style={{borderBottom: ".1em solid black"}}>
+          <span style={{
+            visibility: guessedLetters.includes(letter) ? "visible" : "hidden"
+          }}>{letter}</span>
+        </span>
+      ))}
+    </div>
   )
 }
